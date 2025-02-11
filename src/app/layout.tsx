0044fde33b1b1405beab54b320ef02ea83d2b2a9
Sandor Weisz <sandor@mysteryleague.com>
2025-02-11 14:16:26 -0600

    Change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Wordsmither",
+  title: "Wordplaying",
   description: "Cryptic clue construction helper",
 };
 
